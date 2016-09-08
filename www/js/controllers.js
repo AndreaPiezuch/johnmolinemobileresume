3bf5329e65a4e90232e0f42517ba867c0b5f2934
John Moline <johnmoline@Johns-MacBook-Pro-2.local>
2016-09-08 14:37:55 -0500

    Updated profile view

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -2,6 +2,15 @@ angular.module('mobileresume.controllers', [])
 
 .controller('ProfileCtrl', function($scope) {
   //scope variables
+  $scope.profileInformation = {
+    "name": "John Moline",
+    "address": "2956 Albares",
+    "city": "Grand Prairie",
+    "state": "TX",
+    "zip": "75054",
+    "phone": "(214)400-9400",
+    "description": "Hello and thank you for visiting my resume. I am a Senior Architect that happens to still write alot of code. It is my strong opinion that all senior-level engineers still ensure that they keep on top of technology and how it evolves. To be a successful mentor, one needs to know the ins/outs of all software platforms. Please click the other application tabs for more information on my background. Thank you!"
+  };
   $scope.profileQuote = "";
   $scope.profileListItems = [
     {
