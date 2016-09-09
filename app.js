8310f12451ee9f7857877a1448139c092be8ce10
John Moline <johnmoline@Johns-MacBook-Pro-2.local>
2016-09-09 09:06:01 -0500

    updates for app.js routing and installed cloud foundy module

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 //init modules
 var express = require('express');
+var cfenv = require('cfenv');
 var app = express();
 
-//init root path
+//set all paths for content
+app.use(express.static(__dirname + '/public/www'));
 app.get('/', function(req, res) {
-
+  res.sendFile(__dirname + '/public/www/index.html');
 });
 
+console.log("File path: " + __dirname);
+
+//init cfenv variable
+var appEnv = cfenv.getAppEnv();
+
 //init server listening event
-app.listen(3000, function() {
-  console.log("Example app listenting on port 3000!");
+app.listen(appEnv.port, '0.0.0.0', function() {
+  console.log("Example app listenting on " + appEnv.url);
 });
